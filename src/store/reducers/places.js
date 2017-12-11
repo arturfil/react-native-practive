0325cac996b03078909d7f504c8e098c89963aed
Arturo Filio Villa <arturfil@hotmail.com>
2017-12-11 18:01:13 -0500

    added provider and setup in index.js  along with reducers

--- src/store/reducers/places.js
+++ src/store/reducers/places.js
@@ -1,3 +1,5 @@
+import { ADD_PLACE, DELETE_PLACE, SELECT_PLACE, DESELECT_PLACE } from '../actions/actionTypes'
+
 const initialState = {
     places: [],
     selectedPlace: null
@@ -5,6 +7,38 @@ const initialState = {
 
 const reducer = (state = initialState, action) => {
     switch (action.type) {
+        case ADD_PLACE: 
+            return {
+                ...state,
+                places: state.places.concat({
+                    key: Math.random(),
+                    name: action.placeName,
+                    image: {
+                        uri: 
+                        "https://media-cdn.tripadvisor.com/media/photo-s/0d/9f/de/6b/encantada-tulum.jpg"
+                    }
+                })
+            }
+        case DELETE_PLACE:
+            return {
+                ...state,
+                places: state.places.filter(place => {
+                    return place.key !== state.selectedPlace.key;
+                }),
+                selectedPlace: null
+            }
+        case SELECT_PLACE:
+            return {
+                ...state,
+                selectedPlace: prevState.places.find(place => {
+                    return place.key === action.placeKey;
+                })
+            }
+        case DELETE_PLACE: 
+            return {
+                ...state,
+                selectedPlace: null
+            };
         default: 
             return state;
     }
